433c5c87cd5d76050227c3b4a3d29504b99b20dd
Shoni Pal <100448741+Shoni182@users.noreply.github.com>
2026-01-15 17:32:51 +0200

    added components

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,36 @@
-import css from "./App.module.css";
+// import css from "./App.module.css";
 import CafeInfo from "../CafeInfo/CafeInfo";
+import { useState } from "react";
+import type { Votes } from "../../types/Votes";
+import type { VoteType } from "../../types/Votes";
+import VoteOptions from "../VoteOptions/VoteOptions";
 
 export default function App() {
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const handleVote = (voteName: VoteType) => {
+    setVotes({
+      ...votes,
+      [voteName]: votes[voteName] + 1,
+    });
+  };
+
+  const resetVotes = () => {
+    setVotes({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
   return (
-    <div className={css.app}>
+    <>
       <CafeInfo />
-    </div>
+      <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={true} />
+    </>
   );
 }

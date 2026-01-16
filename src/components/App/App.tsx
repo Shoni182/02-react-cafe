cc7ba84693c394349bb1079339d02b0cb6eff762
Shoni Pal <100448741+Shoni182@users.noreply.github.com>
2026-01-16 02:45:42 +0200

    fix 3

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,8 @@
 import css from "./App.module.css";
 import CafeInfo from "../CafeInfo/CafeInfo";
 import { useState } from "react";
-import type { Votes, VoteType } from "../../../src/types/votes";
+import type { VoteType } from "../../types/votes";
+import type { Votes } from "../../types/votes";
 import VoteOptions from "../VoteOptions/VoteOptions";
 import VoteStats from "../VoteStats/VoteStats";
 import Notification from "../Notification/Notification";
